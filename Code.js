e9ad98006e012a9313e10a2b6305fca2603917ac
Philip Ferenz <philip.ferenz@gmail.com>
2022-04-14 20:30:24 +0200

    added styled sliders

--- Code.js
+++ Code.js
@@ -4,6 +4,12 @@
 //global vars? dunno, lets see what we need
 function init() {
     document.getElementById("iUploadImage").addEventListener('change', processImage);
+
+    var elements = document.getElementsByClassName("reactive_range");
+    for(var element of elements) {
+        element.oninput = adjustRangeVisuals;
+        element.oninput();
+    }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -18,6 +24,17 @@ function acceptFile() {
     //the normal file button is ugly, and styling it is not supported up until recently
     //it is solved that way for the sake of backward compatibility
     document.getElementById('iUploadImage').click();
+}
+
+//adds color to the left and right side of the sliders thumb (visual sugar, nothing more)
+function adjustRangeVisuals() {
+    var value = (this.value-this.min)/(this.max-this.min)*100;
+    this.style.background = 'linear-gradient(' +
+        'to right,' +
+        'var(--color_accent) 0%, var(--color_accent) '+value+'%, ' +
+        'var(--color_background) '+value+'%, var(--color_background) 100%' +
+        ')'
+    ;
 }
 
 //----------------------------------------------------------------------------------------------------------------------

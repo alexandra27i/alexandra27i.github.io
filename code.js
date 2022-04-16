9f60690e0a2cc62d4548d24f3cc661de3c033dab
Philip Ferenz <philip.ferenz@gmail.com>
2022-04-16 23:28:22 +0200

    added basic stippling algorithm (NOT working yet)

--- code.js
+++ code.js
@@ -5,10 +5,13 @@ let d3surface;
 let renderer;
 
 let numStipples = 100;
-let stippleSizeMin = 10;
-let stippleSizeMax = 10;
+let stippleSizeMin = 2;
+let stippleSizeMax = 20;
 let stipples;
 let lastScalingFactor;
+let intensities;
+let intensitiesSize;
+let stipplingReady = false;
 
 function init() {
     d3surface = document.getElementById('d3surface');
@@ -25,9 +28,6 @@ function init() {
         element.oninput = adjustRangeVisuals;
         element.oninput();
     }
-
-    initStipples();
-    redrawStipples();
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -78,13 +78,38 @@ function getScalingFactor() {
 //region Stippling
 //----------------------------------------------------------------------------------------------------------------------
 
-function processImage() {
-    if (this.files && this.files[0]) {
-        var imgdata = this.files[0];
+//called on image upload
+//converts image to intensities and initializes stippling
+function processImage(fileEvent) {
+    if(!fileEvent.target.files[0]) return;
 
-        //var img = document.getElementById("testImage");
-        //img.src = URL.createObjectURL(imgdata);
+    let ca = document.createElement("canvas");
+    let co = ca.getContext("2d");
+
+    let reader = new FileReader();
+    reader.onload = function(readerEvent) {
+        let img = new Image();
+        img.onload = function() {
+            ca.width = img.width;
+            ca.height = img.height;
+            co.drawImage(img, 0, 0);
+
+            let subpixels = co.getImageData(0, 0, ca.width, ca.height).data;
+
+            //grayscales
+            stipplingReady = false;
+            intensities = new Array(subpixels.length/4);
+            for(let i = 0; i < subpixels.length; i+=4) {
+                let avg = (subpixels[i]+subpixels[i+1]+subpixels[i+2])/3;
+                intensities[i/4] = avg;
+            }
+            intensitiesSize = [img.width, img.height];
+            initStipples();
+            stipplingReady = true;
+        }
+        img.src = readerEvent.target.result;
     }
+    reader.readAsDataURL(fileEvent.target.files[0]);
 }
 
 function initStipples() {
@@ -98,13 +123,131 @@ function initStipples() {
     }
 }
 
+let threshold = 0.4;
 function iterateStepStipples() {
+    if(!stipplingReady) return;
+    d3surface.width = d3surface.clientWidth;
+    d3surface.height = d3surface.clientHeight;
 
+    //create voronoi
+    let points = Array(numStipples)
+        .fill()
+        .map((_, i) => ({
+            x: (stipples[i*3]*0.5+0.5)*d3surface.width,
+            y: (stipples[i*3+1]*0.5+0.5)*d3surface.height,
+            intensity: 0, //todo: rename?
+            weightedSumX: 0,
+            weightedSumY: 0,
+            radius: stipples[i*3+2]
+
+        }));
+    let delaunay = d3.Delaunay.from(
+        points,
+        (d) => d.x,
+        (d) => d.y
+    );
+    let voronoi = delaunay.voronoi([0, 0, d3surface.width, d3surface.height]);
+
+    //get intensities for each voronoi cell
+    let cellStippleIndex = 0;
+    for(let x = 0; x < d3surface.width; x++) {
+        for(let y = 0; y < d3surface.height; y++) {
+            cellStippleIndex = delaunay.find(x, y, cellStippleIndex);
+            let cellStipple = points[cellStippleIndex];
+
+            let _x = x/d3surface.width*intensitiesSize[0];
+            let _y = y/d3surface.height*intensitiesSize[1];
+            let _max = intensitiesSize[0]*intensitiesSize[1]-1;
+            let _intensityPos = Math.max(0, Math.min(Math.trunc(_x*intensitiesSize[1] + _y), _max));
+
+            let intensityAtPos = 255.0 - intensities[_intensityPos];
+            cellStipple.intensity += intensityAtPos;
+            cellStipple.weightedSumX += x*intensityAtPos;
+            cellStipple.weightedSumY += y*intensityAtPos;
+        }
+    }
+
+    //delete, split, move
+    let splits = [];
+    let moved = [];
+    let deleted = [];
+    for(let i = 0; i < points.length; i++) {
+        let polygon = voronoi.cellPolygon(i);
+        let cellStipple = points[i];
+
+        //average intensity of stipple over its area
+        let area = Math.abs(d3.polygonArea(polygon)) || 1; //cant be 0
+        let avg_intensity = cellStipple.intensity / area;
+
+        //map intensity to a radius
+        let radius = (avg_intensity / 255.0) * (stippleSizeMax - stippleSizeMin) + stippleSizeMin;
+
+        //calculate thresholds
+        let stippleArea = Math.PI * radius * radius;
+        let threshold_delete = (1.0 - threshold / 2.0) * stippleArea;
+        let threshold_split = (1.0 + threshold / 2.0) * stippleArea;
+
+        if (cellStipple.intensity < threshold_delete*255.0) {
+            //delete (by not doing anything with this)
+            deleted.push(cellStipple);
+        } else if (cellStipple.intensity > threshold_split*255.0) {
+            //split
+            let cellCenter = d3.polygonCentroid(polygon);
+
+            let dist = Math.sqrt(area / Math.PI) / 2.0; //act like poly is a circle and get half the radius
+            let deltaX = cellCenter[0] - cellStipple.x;
+            let deltaY = cellCenter[1] - cellStipple.y;
+            let vectorLength = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+            deltaX /= vectorLength;
+            deltaY /= vectorLength;
+
+            let splitA = [];
+            splitA.x = cellCenter[0] + dist * deltaX;
+            splitA.y = cellCenter[1] + dist * deltaY;
+            splitA.radius = radius;
+            let splitB = [];
+            splitB.x = cellCenter[0] - dist * deltaX;
+            splitB.y = cellCenter[1] - dist * deltaY;
+            splitB.radius = radius;
+
+            splits.push(splitA);
+            splits.push(splitB);
+        } else {
+            //move
+            cellStipple.x = cellStipple.weightedSumX / cellStipple.intensity;
+            cellStipple.y = cellStipple.weightedSumY / cellStipple.intensity;
+            cellStipple.radius = radius;
+            moved.push(cellStipple);
+        }
+    }
+
+    console.log("deleted: " + deleted.length);
+    console.log("splits: " + splits.length);
+    console.log("moved: " + moved.length);
+
+
+    //adjust threshold and rebuild points
+    threshold += 0.01;
+    points.length = moved.length + splits.length;
+    for(let i = 0; i < moved.length; i++) points[i] = moved[i];
+    for(let i = 0; i < splits.length; i++) points[i + moved.length] = splits[i];
+
+    //rebuild stipples from it todo: think about something else, this is actually horrible
+    numStipples = points.length;
+    stipples = new Array(numStipples * 3); //x,y,size for each stipple
+    for (let i = 0; i < points.length; i++) {
+        stipples[i*3] = ((points[i].x / d3surface.width) - 0.5) * 2.0;
+        stipples[i*3+1] = ((points[i].y / d3surface.height) - 0.5) * 2.0;
+        stipples[i*3+2] = points[i].radius;
+    }
+
+    //display them
+    redrawStipples();
 }
 
 function redrawStipples() {
-    renderer.drawVoronoi(stipples, true);
-    renderer.drawDots(stipples, false);
+    renderer.drawDots(stipples, true);
+    renderer.drawVoronoi(stipples, false);
 }
 
 function scaleStipples() {
@@ -171,7 +314,7 @@ class Renderer { //todo: stippling data object as a class, to streamline any buf
         this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, null);
     }
 
-    drawVoronoi(data, clearBefore = true) {
+    drawVoronoi(data, clearBefore = true) { //todo: dont create voronoi here - we need it outside too
         this.#fixCanvas();
         let lines = this.#generateVoronoi(data);
         this.#gl.useProgram(this.#shaderLines);
@@ -255,6 +398,7 @@ class Renderer { //todo: stippling data object as a class, to streamline any buf
             (d) => d.y
         ).voronoi([-1, -1, 1, 1]);
 
+        let _segments = voronoi.render();
         const segments = voronoi.render().split(/M/).slice(1);
         let lines = new Array(segments.length*4);
         for (let i = 0; i < segments.length; i++) { //p1x,p1y L p2x,p2y

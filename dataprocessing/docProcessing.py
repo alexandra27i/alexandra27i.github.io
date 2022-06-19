ae52ceaa50d6763d29494d36a1ac8dccd9ab1ab6
Philip Ferenz <philip.ferenz@gmail.com>
2022-06-19 13:03:10 +0200

    minor adjustments to the documentation page, added base64 image template

--- dataprocessing/docProcessing.py
+++ dataprocessing/docProcessing.py
@@ -4,26 +4,16 @@ from PIL import Image
 import pycountry
 import pandas as pd
 import json
-
+import base64
 
 
 def create_b64_from_image(source, destination):
     print("creating b64 from {} ...".format(source))
 
-    im = Image.open(source, 'r')
-    width, height = im.size
-    pixels = list(im.getdata())
-    result = bytearray(3*width*height)
-
-    for y in range(0, height):
-        for x in range(0, width):
-            pixel = pixels[y * width + x]
-            result[y * width*3 + x*3 + 0] = pixel[0]
-            result[y * width*3 + x*3 + 1] = pixel[1]
-            result[y * width*3 + x*3 + 2] = pixel[2]
-
-    with open(destination, "w", encoding="utf-8") as dest:
-        dest.write(result.hex())
+    with open(source, "rb") as src:
+        encoded_string = base64.b64encode(src.read())
+        with open(destination, "wb") as dest:
+            dest.write(encoded_string)
 
 
 if __name__ == '__main__':
